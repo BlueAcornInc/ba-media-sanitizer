36b2d383598bfcafa1f8d88b39de66064d50a522
Mike Tay <michael.tay@blueacorn.com>
2016-12-22 10:31:05 -0500

    add simplified calculateAstpectRation(), was getting call stack errors. Using new modules, quickly-copy-file and calipers for getting image size. It is more lightweight and does not need to open the image file for reading

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var fs = require('fs-extra');
 var glob = require('glob');
-var lwip = require('lwip');
 var path = require('path');
-var copy = require('copy');
 var mkdirp = require('mkdirp');
+var calipers = require('calipers')('png','jpeg','gif');
+var copyfiles = require('quickly-copy-file');
+
 var commandLineArguments = require('command-line-args');
 
 var options = commandLineArguments([
@@ -12,12 +13,14 @@ var options = commandLineArguments([
   {name: 'samplesize', alias: 's', type: Number},
   {name: 'files', type: String, multiple: true, defaultOption: true}
 ]).parse();
+
 var mediaDir = options.files[0],
     copyMediaDir = options.files[1];
 
 if (!mediaDir || !copyMediaDir) {
   throw new Error("Must provide arguments for existing media directory and target location.");
 }
+
 var sampleSize = options.samplesize || 10,
     batchSize = options.batchsize || 100,
     verbose = options.verbose;
@@ -43,87 +46,98 @@ var blacklistExtensions = [
   '.c'
 ];
 
+var calculateAspectRation = function(width, height) {
+  return width/height;
+};
+
+var getRandomCopiedImage = function(images, width, height) {
+  var index = Math.floor(images[width][height].length * Math.random());
+  return images[width][height][index];
+};
+
+
 
 if (!fs.existsSync(copyMediaDir)){
-  mkdirp(copyMediaDir, function(err){
+  mkdirp.sync(copyMediaDir, function(err){
     if (err) { console.error(err) }
     else if (verbose) { console.log('Created new directory: ' + copyMediaDir)}
   });
 }
 
-var calculateAspectRation = function(width, height) {
-  return (height == 0) ? width : calculateAspectRation(height, width%height);
-}
-
 glob(mediaDir + '/**/*', {nodir: true}, function(err, files) {
   files.forEach(function(file) {
     var copyFile = copyMediaDir + file.replace(mediaDir, '');
-    var copyFileDir = copyMediaDir + path.dirname(file).replace(mediaDir, '');
-
     // Skip blacklisted items
     if (blacklistExtensions.some(function(ext){return ext == path.extname(file);})) {
       if (verbose) console.log("Skipping file because its extension is blacklisted: " + file);
       return;
     }
-
-    // While 'fs-sync' is awesome, this is still necessary for image.writeFile
-    if (!fs.existsSync(copyFileDir)) {
-      mkdirp(copyFileDir, function(err){
-        if (err) { console.error(err) }
-        else if (verbose) { console.log('Created new directories: ' + copyFileDir)}
-      });
-    }
-
     // If the file is not an image file, just copy it
     if (!imgExtensions.some(function(ext){return ext == path.extname(file);})) {
       //fs does not have a copy method, need fs-extra
-      copy.one(file, copyFileDir, function(err,file){
+      copyfiles(file, copyFile, function(err,file){
         if(err) { console.error(err) }
-        else if (verbose) { console.log("Copying non-image file: " + file + " to " + copyFileDir)}
+        else if (verbose) { console.log("Copying non-image file: " + file + " to " + copyFile)}
       });
       return;
     }
 
-    fs.readFile(file, function(err,buffer) {
+    calipers.measure(file,function(err,result) {
+      if(err){console.error(err)}
+      if(result == undefined) {
+        return console.log('Result was undefined, could not read file: ' + file);
+      }
+      var width = result.pages.width,
+          height = result.pages.height;
 
-      lwip.open(buffer, path.extname(file).substring(1), function (err,image) {
-        if (err) {
-          return console.error(err);
+      var aspectRation = calculateAspectRation(width, height);
+
+      width = width / aspectRation;
+      height = height / aspectRation;
+
+      if (!images.hasOwnProperty(width)) {
+        images[width] = {};
+      }
+      if (!images[width].hasOwnProperty(height)) {
+        images[width][height] = [];
+      }
+      if (images[width][height].length < sampleSize) {
+        // Copy image
+        images[width][height].push(copyFile);
+
+        copyfiles(file, copyFile, function (err, file) {
+          if (err) {
+            console.error(err)
+          }
+          else if (verbose) console.error("Copying image file: " + file + " to " + copyFile);
+
+        });
+      } else {
+        // Create link to random previously copied image
+        var randomImage = getRandomCopiedImage(images,width,height);
+
+        var counter = 0;
+        while(!fs.existsSync(randomImage) && counter < 100) {
+          randomImage = getRandomCopiedImage(images, width, height);
+          counter++;
+        }
+        if (counter > 100) {
+          console.error('Counter reached 100 iterations while trying to find a copied image.')
         }
 
-        var width = image.width(),
-            height = image.height();
-    
-        var aspectRation = calculateAspectRation(width, height);
-
-        width = width/aspectRation;
-        height = height/aspectRation;
-
-        if (!images.hasOwnProperty(width)) {
-          images[width] = {};
-        }
-        if (!images[width].hasOwnProperty(height)) {
-          images[width][height] = [];
-        }
-
-        if (images[width][height].length < sampleSize) {
-          // Copy image
-          images[width][height].push(copyFile);
-          copy.one(file, copyFileDir, function(err,file) {
-            if (err) {console.error(err)}
-            else if (verbose) console.log("Copying image file: " + file + " to " + copy);
-
-          });
-        } else {
-          // Create link to random previously copied image
-          var randomIndex = Math.floor(images[width][height].length * Math.random()),
-              randomImage = images[width][height][randomIndex];
-
-          fs.link(randomImage, copyFile);
-          if (verbose) console.log("Linking image file: " + randomImage + " to " + copyFile)
-        }
-
-      });
+        fs.ensureLink(randomImage, copyFile, function (err) {
+          if (err) {
+            console.log('Random image: ' + randomImage);
+            console.log('copyFile: ' + copyFile);
+            console.log(images);
+            console.error(err)
+          }
+          else if (verbose) {
+            console.log("Linking image file: " + randomImage + " to " + copyFile)
+          }
+        });
+      }
     });
+
   });
 });
